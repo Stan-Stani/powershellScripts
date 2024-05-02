eee072fd51ef7cbf700924cf9d05a2fbfff51f79
Stan Stanislaus <stan.stanislaus@steffesgroup.com>
2024-05-02 14:08:36 -0500

    feat: start wifi hotspot programmatically

--- ngrokFreeApply/ngrokFreeApply.ps1
+++ ngrokFreeApply/ngrokFreeApply.ps1
@@ -1,4 +1,7 @@
 
+
+
+
 function Get-Common-Line-Values {
     param (
         [string]
@@ -122,3 +125,41 @@ function Start-Ngrok {
 }
 
 Start-Ngrok
+
+$session = New-PSSession -UseWindowsPowerShell
+Invoke-Command -Session $session {
+
+    # Src https://superuser.com/questions/1341997/using-a-uwp-api-namespace-in-powershell
+    Add-Type -AssemblyName System.Runtime.WindowsRuntime
+    $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+    Function Await($WinRtTask, $ResultType) {
+        $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+        $netTask = $asTask.Invoke($null, @($WinRtTask))
+        $netTask.Wait(-1) | Out-Null
+        $netTask.Result
+    }
+    
+
+    # Src https://stackoverflow.com/questions/45833873/enable-windows-10-built-in-hotspot-by-cmd-batch-powershell
+    Function Start-Hotspot() {
+        $connectionProfile = [Windows.Networking.Connectivity.NetworkInformation, Windows.Networking.Connectivity, ContentType = WindowsRuntime]::GetInternetConnectionProfile()
+        $tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager, Windows.Networking.NetworkOperators, ContentType = WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)
+
+        # Be sure to include Ben N.'s await for IAsyncOperation:
+        # https://superuser.com/questions/1341997/using-a-uwp-api-namespace-in-powershell
+
+
+        
+        # Check whether Mobile Hotspot is enabled
+        $tetheringManager.TetheringOperationalState
+
+        # Start Mobile Hotspot
+        Await ($tetheringManager.StartTetheringAsync()) ([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
+
+        # Stop Mobile Hotspot
+        # Await ($tetheringManager.StopTetheringAsync()) ([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
+    }
+
+
+    Start-Hotspot
+}
