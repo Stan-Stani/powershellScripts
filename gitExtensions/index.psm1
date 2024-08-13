bab1eb7ee251b8e3a3c7767b732470a256483c2b
Stan Stanislaus <stan.stanislaus@steffesgroup.com>
2024-08-13 17:22:26 -0500

    feat: improve pager functionality

--- gitExtensions/index.psm1
+++ gitExtensions/index.psm1
@@ -1,4 +1,31 @@
 function Invoke-GitExtensionCheckoutHistory {
-    git reflog | Select-String 'checkout' | ForEach-Object { $_ -replace '^.*to ', '' } | Out-Host -Paging
+    $history = git reflog | Select-String 'checkout' | ForEach-Object { $_ -replace '^.*to ', '' }
+    
+    $lessCommand = Get-Command less -ErrorAction SilentlyContinue
+    $chocoCommand = Get-Command choco -ErrorAction SilentlyContinue
+
+    $pager = if ($lessCommand) {
+        $lessCommand.Name
+    }
+    elseif ($env:PAGER) {
+        $env:PAGER
+    }
+    else {
+        $null
+    }
+
+
+
+    if ($pager) {
+        $history | Out-String | & $pager
+    }
+    elseif ($chocoCommand) {
+        $chocoProcess = Start-Process 'pwsh' -Verb RunAs -ArgumentList "-Command choco install less" -PassThru
+        Read-Host -Prompt "Installing ``less``. Press enter to refresh environment variables after choco finishes the install."
+        refreshenv
+    }
+    else {
+        $history | more
+    }
 }
 New-Alias -Name gch -Value Invoke-GitExtensionCheckoutHistory
