0aa8a3616bf22eefb64a61325aa8fea08f5ac22c
Stan Stanislaus <stan.stanislaus@steffesgroup.com>
2024-08-28 12:38:04 -0500

    feat: add git-tui diff func and alias

--- gitExtensions/index.psm1
+++ gitExtensions/index.psm1
@@ -2,7 +2,7 @@ function Invoke-GitExtensionCheckoutHistory {
     $history = git reflog | Select-String 'checkout' | ForEach-Object { 
         [PSCustomObject]@{
             Checkout = $_ -replace '^.*to ', ''
-            Index  = $index++
+            Index    = $index++
         } 
     }
     
@@ -25,7 +25,7 @@ function Invoke-GitExtensionCheckoutHistory {
         $history | Out-String | & $pager
     }
     elseif ($chocoCommand) {
-        $chocoProcess = Start-Process 'pwsh' -Verb RunAs -ArgumentList "-Command choco install less" -PassThru
+        Start-Process 'pwsh' -Verb RunAs -ArgumentList "-Command choco install less" -PassThru
         Read-Host -Prompt "Installing ``less``. Press enter to refresh environment variables after choco finishes the install."
         refreshenv
     }
@@ -34,3 +34,24 @@ function Invoke-GitExtensionCheckoutHistory {
     }
 }
 New-Alias -Name gch -Value Invoke-GitExtensionCheckoutHistory
+
+function Invoke-GitExtensionTUIDiff {
+    $gitTUICommand = Get-Command git-tui -ErrorAction SilentlyContinue
+    $chocoCommand = Get-Command choco -ErrorAction SilentlyContinue
+    if ($gitTUICommand) {
+        # `saps` aliases Start-Process
+        # @todo figure out how to start maximized...
+        saps pwsh "-NoExit -Command git-tui diff ${$args[0]}"
+    }
+    elseif ($chocoCommand) {
+        Start-Process 'pwsh' -Verb RunAs -ArgumentList "-Command choco install gittui" -PassThru
+        Read-Host -Prompt "Installing ``git-tui``. Press enter to refresh environment variables after choco finishes the install."
+        refreshenv
+        saps pwsh "-NoExit -Command git-tui diff ${$args[0]}"
+    } else {
+        Write-Host "Couldn't install git-tui because Chocolately isn't installed. Do that first!"
+        Write-Host ""
+        Write-Host "You'll just have to use good ol' ``git diff `"blah`"``"
+    }
+}
+New-Alias -Name gtd -Value Invoke-GitExtensionTUIDiff
